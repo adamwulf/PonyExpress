6dfaf6de91dd97c0ab6ffcc89ad20b3bfd09b51b
Adam Wulf <adam.wulf@gmail.com>
2022-12-13 14:24:46 -0600

    Use upToNextMajor for Locks repo

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/adamwulf/Locks.git", branch: "main")
+        .package(url: "https://github.com/adamwulf/Locks.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
