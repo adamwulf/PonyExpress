6247bd525b59337f8e8296ac1538dfd0ba2cee79
Adam Wulf <adam.wulf@gmail.com>
2022-12-30 20:34:00 -0600

    Prepped for registering unmarked blocks

--- Tests/PonyExpressTests/UnmarkedMailBlockTests.swift
+++ Tests/PonyExpressTests/UnmarkedMailBlockTests.swift
@@ -3,11 +3,115 @@ import XCTest
 
 final class UnmarkedMailBlockTests: XCTestCase {
 
+    func testUnmarkedRequiredSenderBlock() throws {
+        let notification = ExampleUnmarked(info: 1, other: 2)
+        let sender1 = UnmarkedSender()
+        let sender2 = UnmarkedSender()
+        let postOffice = PostOffice()
+        var count = 0
+
+        postOffice.register { (_: ExampleUnmarked, _: UnmarkedSender) in
+            count += 1
+        }
+
+        postOffice.post(notification, sender: sender1)
+        postOffice.post(notification, sender: sender2)
+
+        XCTAssertEqual(count, 2)
+    }
+
+    func testUnmarkedOptionalSenderBlock() throws {
+        let notification = ExampleUnmarked(info: 1, other: 2)
+        let sender1 = UnmarkedSender()
+        let sender2 = UnmarkedSender()
+        let postOffice = PostOffice()
+        var count = 0
+
+        postOffice.register { (_: ExampleUnmarked, _: UnmarkedSender) in
+            count += 1
+        }
+
+        postOffice.post(notification, sender: sender1)
+        postOffice.post(notification, sender: sender2)
+
+        XCTAssertEqual(count, 2)
+    }
+
+    func testUnmarkedSpecificSenderBlock() throws {
+        let notification = ExampleUnmarked(info: 1, other: 2)
+        let sender1 = UnmarkedSender()
+        let sender2 = UnmarkedSender()
+        let postOffice = PostOffice()
+        var count = 0
+
+        postOffice.register(sender: sender1) { (_: ExampleUnmarked, _: UnmarkedSender) in
+            count += 1
+        }
+
+        postOffice.post(notification, sender: sender1)
+        postOffice.post(notification, sender: sender2)
+
+        XCTAssertEqual(count, 1)
+    }
+
+    func testUnmarkedSpecificQueueBlock() throws {
+        let bgQueue = DispatchQueue(label: "test.queue")
+        let notification = ExampleUnmarked(info: 1, other: 2)
+        let sender1 = UnmarkedSender()
+        let postOffice = PostOffice()
+        var count = 0
+
+        postOffice.register(queue: bgQueue) { (_: ExampleUnmarked, _: UnmarkedSender) in
+            count += 1
+        }
+
+        postOffice.post(notification, sender: sender1)
+
+        let exp = expectation(description: "wait for notification")
+
+        bgQueue.sync {
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 0.1)
+
+        XCTAssertEqual(count, 1)
+    }
+
+    func testUnmarkedIgnoredSender() throws {
+        let notification = ExampleUnmarked(info: 1, other: 2)
+        let sender1 = UnmarkedSender()
+        let postOffice = PostOffice()
+        var count = 0
+
+        postOffice.register { (_: ExampleUnmarked) in
+            count += 1
+        }
+
+        postOffice.post(notification, sender: sender1)
+
+        XCTAssertEqual(count, 1)
+    }
+
+    func testUnmarkedIgnoredSender2() throws {
+        let notification = ExampleUnmarked(info: 1, other: 2)
+        let sender1 = UnmarkedSender()
+        let postOffice = PostOffice()
+        var count = 0
+
+        postOffice.register(sender: sender1) { (_: ExampleUnmarked) in
+            count += 1
+        }
+
+        postOffice.post(notification, sender: sender1)
+
+        XCTAssertEqual(count, 1)
+    }
 //    func testMatchSenderTypeOptional() throws {
 //        struct ExampleUnmarked: UnmarkedMail {
 //            var info: Int
 //        }
-//        struct ExamplePostmarked: PostmarkedMail {
+//        struct ExampleUnmarked: UnmarkedMail {
 //            var info: Int
 //        }
 //
@@ -18,18 +122,18 @@ final class UnmarkedMailBlockTests: XCTestCase {
 //        postOffice.register { (_: ExampleMail) -> Void in
 //            received += 1
 //        }
-//        postOffice.register { (_: ExamplePostmarked) -> Void in
+//        postOffice.register { (_: ExampleUnmarked) -> Void in
 //            received += 1
 //        }
 //
-//        postOffice.post(ExamplePostmarked(info: 12), sender: sender)
+//        postOffice.post(ExampleUnmarked(info: 12), sender: sender)
 //        postOffice.post(ExampleMail(info: 12))
 //
-//        // compiler error, Postmarked notifications that do not implement Mail /must/ be sent with a Sender
-//        // postOffice.post(ExamplePostmarked(info: 12))
-//        // postOffice.post(ExamplePostmarked(info: 12), sender: nil)
+//        // compiler error, Unmarked notifications that do not implement Mail /must/ be sent with a Sender
+//        // postOffice.post(ExampleUnmarked(info: 12))
+//        // postOffice.post(ExampleUnmarked(info: 12), sender: nil)
 //        // let emptySender: NSObject? = nil
-//        // postOffice.post(ExamplePostmarked(info: 12), sender: emptySender)
+//        // postOffice.post(ExampleUnmarked(info: 12), sender: emptySender)
 //
 //        XCTAssertEqual(received, 2)
 //    }
